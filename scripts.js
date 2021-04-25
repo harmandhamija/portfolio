d7ee61696146607e46e09c489e76106de0cea1c0
harman <harmanave@gmail.com>
2021-04-25 23:36:14 +0530

    added media queries

--- scripts.js
+++ scripts.js
@@ -1,14 +1,50 @@
+const myApp = {};
 
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const menuList = document.querySelector('.menu-list');
-const ulElement = document.querySelector('ul');
+myApp.toggleMenu = function(){
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const menuList = document.querySelector('.menu-list');
+    const ulElement = document.querySelector('ul');
 
-hamburgerMenu.addEventListener('click', function () {
-    hamburgerMenu.classList.toggle('toggle');
-    menuList.classList.toggle('menu-list-show');
-})
+    hamburgerMenu.addEventListener('click', function () {
+        hamburgerMenu.classList.toggle('toggle');
+        menuList.classList.toggle('show');
+    })
 
-ulElement.addEventListener('click', function () {
-    hamburgerMenu.classList.remove('toggle');
-    menuList.classList.remove('menu-list-show');
-})
+    ulElement.addEventListener('click', function () {
+        hamburgerMenu.classList.remove('toggle');
+        menuList.classList.remove('show');
+    })
+}
+
+// scroll to top button
+
+myApp.handleScroll = function () {
+
+    const scrollButton = document.querySelector('.scroll-top');
+    const rootEl = document.documentElement;
+    const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight;
+
+    document.addEventListener('scroll', function() {
+        if ((rootEl.scrollTop / scrollTotal) > 0.20) {
+            scrollButton.classList.add('active')
+        } else {
+            scrollButton.classList.remove('active')
+        }
+    })
+
+    scrollButton.addEventListener('click', function() {
+        rootEl.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    })
+}
+
+myApp.init = function(){
+    myApp.toggleMenu();
+    AOS.init();
+    myApp.handleScroll();
+}
+
+myApp.init();
+
